c636688d06dd5d415964d3605994191a51ae6ae3
Janna-kelly <54596904+Janna-kelly@users.noreply.github.com>
2019-09-12 12:37:05 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,336 @@
+function setup() { 
+    createCanvas(400,269);
+}
+
+function draw() {
+    background(250);
+    strokeWeight(4);
+    stroke(250);
+
+    for (var x = 0; x <= innerWidth; x += 6) {
+        for (var y = 0; y <= innerHeight; y += 6) {
+            fill(178,34,34);
+            ellipse(x, y, 9, 9);
+        }
+    }
+   
+    fill(0)
+    strokeWeight (0);
+    curve (120,226,140,200,120,226,230,226)
+    fill(0)
+    strokeWeight(0);
+    curve (115,300,130,210,265,275,22,340)
+    fill(0)
+    strokeWeight (0);
+    curve (120,226,230,275,300,250,230,226)
+    fill(0)
+    strokeWeight (0);
+    curve (250,125,205,70,230,120,180,170)
+    fill(0)
+    strokeWeight (0);
+    curve (250,125,215,70,240,115,180,170)
+    fill(0)
+    strokeWeight (0);
+    curve (250,125,222,70,250,110,180,170)
+    fill(0)
+    strokeWeight (0);
+    curve (290,125,230,68,260,105,180,170)
+    fill(0)
+    strokeWeight (0);
+    curve (320,125,236,65,270,100,180,170)
+   
+   
+    stroke(255)
+    strokeWeight(23)
+    line(370.9,129.26,404.08,129.26)
+    stroke(255)
+    strokeWeight(5)
+    line(336.65,202.27,404.08,202.27)
+    noFill();
+    strokeWeight(8);
+    stroke(0)
+    beginShape();
+    curveVertex(372,-5.6);
+    curveVertex(372,-5.6);
+    curveVertex(359,10);
+    curveVertex(353,29);
+    curveVertex(356,75);
+    curveVertex(362,113);
+    curveVertex(370,160);
+    curveVertex(367,175);
+    curveVertex(343,195);
+    curveVertex(322,203);
+    curveVertex(296,203);
+    curveVertex(279,222);
+    curveVertex(275,249);
+    curveVertex(275,249);
+    endShape();
+
+    noFill();
+    strokeWeight(6);
+    stroke(0)
+    beginShape();
+    curveVertex(175,35);
+    curveVertex(175,35);
+    curveVertex(184.4,40.86);
+    curveVertex(211.35,50.67);
+    curveVertex(247.72,42.69);
+    curveVertex(247.72,42.69);
+    endShape(); 
+   
+    noFill();
+    strokeWeight(6);
+    stroke(0)
+    beginShape();
+    curveVertex(255.77,167.89);
+    curveVertex(255.77,167.89);
+    curveVertex(256.83,184.83);
+    curveVertex(265.69,198.89);
+    curveVertex(265.69,198.89);
+    endShape(); 
+    
+    noFill();
+    strokeWeight(7);
+    stroke(0)
+    beginShape();
+    curveVertex(299.88,203.61);
+    curveVertex(299.88,203.61);
+    curveVertex(289.75,232.68);
+    curveVertex(291.84,258.75);
+    curveVertex(291.84,258.75);
+    endShape(); 
+    
+    noFill();
+    strokeWeight(10);
+    stroke(0)
+    beginShape();
+    curveVertex(360.81,-1.97);
+    curveVertex(360.81,-1.97);
+    curveVertex(347.52,21.8);
+    curveVertex(349.78,50.15);
+    curveVertex(368.85,154.34);
+    curveVertex(368.85,154.34);
+    endShape(); 
+    
+    noFill();
+    strokeWeight(6);
+    stroke(0)
+    beginShape();
+    curveVertex(359,118.41);
+    curveVertex(359,118.41);
+    curveVertex(367.87,103.86);
+    curveVertex(378,80.94);
+    curveVertex(381.27,55.27);
+    curveVertex(381.27,55.27);
+    endShape(); 
+
+    strokeWeight(3);
+    stroke(255)
+    beginShape();
+    curveVertex(381.52,53.49);
+    curveVertex(381.52,53.49);
+    curveVertex(386.85,36.39);
+    curveVertex(377,35.07);
+    curveVertex(368.81,55.42);
+    curveVertex(380.49,54.71);
+    curveVertex(380.49,54.71);
+    endShape(); 
+    
+    noFill();
+    strokeWeight(6);
+    stroke(0)
+    beginShape();
+    curveVertex(352.95,52.05);
+    curveVertex(352.95,52.05);
+    curveVertex(367.27,55.94);
+    curveVertex(381.27,55.27);
+    curveVertex(381.27,55.27);
+    endShape(); 
+
+    noFill();
+    strokeWeight(10);
+    stroke(0)
+    beginShape();
+    curveVertex(349.3,35.08);
+    curveVertex(349.3,35.08);
+    curveVertex(375.12,32.4);
+    curveVertex(400.95,38.3);
+    curveVertex(400.95,38.3);
+    endShape(); 
+
+    noFill();
+    strokeWeight(10);
+    stroke(0)
+    beginShape();
+    curveVertex(255.77,167.89);
+    curveVertex(255.77,167.89);
+    curveVertex(256.83,184.83);
+    curveVertex(260.69,193.85);
+    curveVertex(260.69,193.85);
+    endShape(); 
+    strokeWeight(7)
+    line(347.33,194.01,403.33,194.01);
+    strokeWeight(18)
+    line(293.97,257.03,403.01,257.03)
+    strokeWeight(4)
+    line(365.73,110.56,401,110.56)
+
+    noFill();
+    strokeWeight(8);
+    stroke(0)
+    beginShape();
+    curveVertex(281.06,-7.13);
+    curveVertex(281.06,-7.13);
+    curveVertex(301.76,2.85);
+    curveVertex(317.79,1.64);
+    curveVertex(334.4,-1.93);
+    curveVertex(334.4,-6);
+    endShape(); 
+
+
+
+    noFill();
+    strokeWeight(4);
+    stroke(0)
+    beginShape();
+    curveVertex(351.78,21.79);
+    curveVertex(351.78,21.79);
+    curveVertex(365.13,19.59);
+    curveVertex(378.49,22.12);
+    curveVertex(378.49,22.12);
+    endShape(); 
+   
+    noFill();
+    strokeWeight(6);
+    stroke(0)
+    beginShape();
+    curveVertex(392.6,37.77);
+    curveVertex(392.6,37.77);
+    curveVertex(403.98,13.99);
+    curveVertex(403.98,13.99);
+    endShape(); 
+    
+    fill(255);
+    strokeWeight(6);
+    stroke(255)
+    beginShape();
+    curveVertex(253.26,11.51);
+    curveVertex(253.26,11.51);
+    curveVertex(241.54,26.43);
+    curveVertex(232.58,25.02);
+    curveVertex(165.01,-0.91);
+    curveVertex(185.62,-27.67);
+    curveVertex(223.53,-5.71);
+    curveVertex(253.26,11.51);
+    curveVertex(253.26,11.51);
+    endShape(); 
+
+    noFill();
+    strokeWeight(6);
+    stroke(0)
+    beginShape();
+    curveVertex(381.52,53.49);
+    curveVertex(381.52,53.49);
+    curveVertex(386.85,36.39);
+    curveVertex(386.85,36.39);
+    endShape(); 
+
+    fill(0)
+    strokeWeight(3);
+    stroke(0)
+    beginShape();
+    curveVertex(363.5,10.35);
+    curveVertex(363.5,10.35);
+    curveVertex(388.64,-0.99);
+    curveVertex(390.54,11.68);
+    curveVertex(403.31,-6.64);
+    curveVertex(405.41,18.73);
+    curveVertex(363.5,10.35);
+    curveVertex(363.5,10.35);
+    endShape(); 
+    
+
+    fill(0)
+    strokeWeight(3);
+    stroke(0)
+    beginShape();
+    curveVertex(349.29,40.26);
+    curveVertex(349.29,40.26);
+    curveVertex(359.14,39.57);
+    curveVertex(377,35.07);
+    curveVertex(368.81,55.42);
+    curveVertex(352.95,52.05);
+    curveVertex(349.29,50.15);
+    curveVertex(349.29,50.15);
+    endShape(); 
+
+    strokeWeight(4)
+    line(266.33,150.33,285.12,157.42);
+    strokeWeight(3)
+    line(273.8,146.65,289.6,152.52);
+    strokeWeight(3)
+    line(281.2,141.15,300.88,148.64);
+    strokeWeight(4)
+    line(287.05,136.62,308.98,144.37);
+    
+  
+    strokeWeight(3);
+    stroke(0)
+    beginShape();
+    curveVertex(326.52,190.33);
+    curveVertex(326.52,190.33);
+    curveVertex(308.74,184.55);
+    curveVertex(298.03,182.96);
+    curveVertex(269.49,188.81);
+    curveVertex(289.08,190.92);
+    curveVertex(300.55,182.52);
+    curveVertex(308.74,184.55);
+    curveVertex(326.52,190.33);
+    curveVertex(326.52,190.33);
+    endShape(); 
+
+    strokeWeight(10);
+    stroke(0)
+    beginShape();
+    curveVertex(277.58,19.32);
+    curveVertex(277.58,19.32);
+    curveVertex(249.36,8.19);
+    curveVertex(230.58,-3.34);
+    curveVertex(230.58,-3.34);
+    endShape(); 
+
+    strokeWeight(10);
+    stroke(0)
+    beginShape();
+    curveVertex(256.4,12.03);
+    curveVertex(285.75,-1.62);
+    curveVertex(266.93,-4.48);
+    curveVertex(251.18,7.96);
+    curveVertex(256.4,12.03);
+    endShape(); 
+
+    strokeWeight(6);
+    stroke(0)
+    beginShape();
+    curveVertex(214.4,-.59);
+    curveVertex(214.4,-.59);
+    curveVertex(209.28,7.15);
+    curveVertex(197.22,12.78);
+    curveVertex(197.22,12.78);
+    endShape();
+    
+    strokeWeight(6);
+    stroke(0)
+    beginShape();
+    curveVertex(210.07,-10.6);
+    curveVertex(210.07,-10.6);
+    curveVertex(192.02,7.49);
+    curveVertex(161.17,-4.14);
+    curveVertex(210.07,-10.6);
+    curveVertex(210.07,-10.6);
+    endShape();
+  }
+
+
+
+
